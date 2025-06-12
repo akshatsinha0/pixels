29aa958812cc25caa19ec724bca8c8e1d7314536
Akshat Sinha <akshatsinhasramhardy@gmail.com>
2025-06-12 17:00:48 +0530

    success

--- src/app/components/BoardMembers.js
+++ src/app/components/BoardMembers.js
@@ -1,6 +1,7 @@
 'use client'
 import { useEffect, useRef, useState } from 'react'
 import Image from 'next/image'
+import gsap from 'gsap' 
 
 export default function BoardMembers() {
   const boardRef = useRef(null)
